67897809e1aef3207b5386851b3a454e3cf170ca
rainerhosch <rz.oktan@gmail.com>
2025-02-28 11:47:22 +0700

    Update social media links in Footer component

--- dapp/src/components/Footer.tsx
+++ dapp/src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <>
             <nav className="relative bottom-0 left-0 right-0 bg-black/50 backdrop-blur-md">
                 <div className="grid grid-cols-4 gap-1 p-2">
-                    <a href="https://twitter.com/PenguinOnchain" target="_blank" className="flex flex-col items-center p-2 text-white hover:text-black ">
+                    <a href="https://x.com/onchainpengo" target="_blank" className="flex flex-col items-center p-2 text-white hover:text-black ">
                         <svg className="w-6 h-6" viewBox="0 0 24 24" fill="currentColor">
                             <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
                         </svg>
@@ -26,7 +26,7 @@ export default function Footer() {
                         <span className="text-xs mt-1">Luma</span>
                     </a>
 
-                    <a href="https://youtube.com/@PenguinOnchain" target="_blank" className="flex flex-col items-center p-2 text-white hover:text-black ">
+                    <a href="https://www.youtube.com/@penguinonchain" target="_blank" className="flex flex-col items-center p-2 text-white hover:text-black ">
                         <svg className="w-6 h-6" viewBox="0 0 24 24" fill="currentColor">
                             <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122-2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
                         </svg>
